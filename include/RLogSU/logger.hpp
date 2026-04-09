2d4e2a9c1ce9dfc05d0124144f18d2cb80cdaf94
71frukt <71frukt@mail.ru>
2026-04-09 13:20:43 +0300

    assert fix

--- include/RLogSU/logger.hpp
+++ include/RLogSU/logger.hpp
@@ -28,9 +28,9 @@ inline void RLSU_THROW(std::string message, std::source_location loc = std::sour
     #define RLSU_MESSAGE( std_format_, ...) do {RLSU::Log::UnitLogger.Log(__FILE__, __LINE__, __func__, RLSU::Log::Logger::MESSAGE, std_format_, ##__VA_ARGS__);} while(0)
     #define RLSU_LOG(     std_format_, ...) do {RLSU::Log::UnitLogger.Log(__FILE__, __LINE__, __func__, RLSU::Log::Logger::LOG    , std_format_, ##__VA_ARGS__);} while(0)
 
-    #define RLSU_ASSERT(condition, std_format_, ...) do {                                                                                              \
+    #define RLSU_ASSERT(condition, ...) do {                                                                                              \
         if (!(condition)) {                                                                                                                            \
-            RLSU::Log::UnitLogger.Log(__FILE__, __LINE__, __func__, RLSU::Log::Logger::ASSERT , #condition " failed. " std_format_, ##__VA_ARGS__);    \
+            RLSU::Log::UnitLogger.Log(__FILE__, __LINE__, __func__, RLSU::Log::Logger::ASSERT , #condition " failed. " __VA_OPT__(__VA_ARGS__));    \
             abort();                                                                                                                                   \
         }                                                                                                                                              \
     } while(0)
@@ -50,7 +50,7 @@ inline void RLSU_THROW(std::string message, std::source_location loc = std::sour
     #define RLSU_INFO(    std_format_, ...)
     #define RLSU_MESSAGE( std_format_, ...)
     #define RLSU_LOG(     std_format_, ...)
-    #define RLSU_ASSERT(condition, std_format_, ...)
+    #define RLSU_ASSERT(condition, ...)
     #define RLSU_DUMP(_DumpFunc, ...)
     #define RLSU_SET_LOGSPACE(path_str)
 #endif
